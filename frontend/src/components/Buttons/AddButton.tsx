a6b59b9b4afc54852867510083a9edf1d82725bb
marinadiniz-plank <marina.diniz@joinplank.com>
2023-05-23 17:38:08 -0300

    updating add button (a pedido de Nander)

--- frontend/src/components/Buttons/AddButton.tsx
+++ frontend/src/components/Buttons/AddButton.tsx
@@ -1,27 +1,26 @@
 import { useState } from 'react';
-import '../../assets/CSS/buttons.css'
+import '../../assets/CSS/buttons.css';
 import AddModal from '../Modal/AddModal';
 
 type ModalProps = {
-    entityName: string,
-    formLabels: string[],
-    onSubmit: () => void
+  entityName: string,
+  formLabels: string[],
+  onSubmit: () => void
 }
 
 
-const AddButton: React.FC<ModalProps> = ({entityName, formLabels, onSubmit }) => {
+const AddButton: React.FC<ModalProps> = ({ entityName, formLabels, onSubmit }) => {
 
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <button 
-          type="submit"
-          className="add_btn"
-          onClick={() => { setIsOpen(true) }}>
-          <i className="fas fa-light fa-plus"></i>
-            add
+      <button
+        type="submit"
+        className="add_btn"
+        onClick={() => { setIsOpen(true) }}>
+        Add
       </button>
-      {isOpen && <AddModal isOpen={isOpen} setIsOpen={setIsOpen} title={entityName} formLabels={formLabels} onSubmit={onSubmit}/>}
+      {isOpen && <AddModal isOpen={isOpen} setIsOpen={setIsOpen} title={entityName} formLabels={formLabels} onSubmit={onSubmit} />}
     </>
   );
 }
