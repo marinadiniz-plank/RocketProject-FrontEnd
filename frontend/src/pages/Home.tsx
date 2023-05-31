f45e3a1119b7be1e0e398e5b37d64fb3c692d1d8
marinadiniz-plank <marina.diniz@joinplank.com>
2023-05-31 13:42:04 -0300

    changing logo and banner

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -8,10 +8,11 @@ function Home() {
 			<Header pageTitle="RocketProject" />
 			<div className="banner">
 				<h4>
-					Existe uma teoria que diz que, se um dia alguém descobrir exatamente para
-					que serve o Universo e por que ele está aqui, ele desaparecerá
-					instantaneamente e será substituído por algo ainda mais estranho e
-					inexplicável. Existe uma segunda teoria que diz que isso já aconteceu.
+					"There is a theory which states that if ever anyone discovers
+					exactly what the Universe is for and why it is here, it will
+					instantly disappear and be replaced by something even more
+					bizarre and inexplicable. There is another theory which states
+					that this has already happened." - Douglas Adams.
 				</h4>
 				<img className="logo" src={bannerImg} alt="React Logo" />
 			</div>
