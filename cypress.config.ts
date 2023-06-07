4cbb20c875226b7798f2af637fbc3a87dfa78ced
marinadiniz-plank <marina.diniz@joinplank.com>
2023-06-07 10:08:04 -0300

    adjusting tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,19 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: "hztvv2",
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    baseUrl:
-      "https://rocketproject-frontend-ad39sjyeb-marinadiniz-plank.vercel.app/",
-  },
+	projectId: "hztvv2",
+	e2e: {
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+		baseUrl:
+			"https://rocketproject-frontend-ad39sjyeb-marinadiniz-plank.vercel.app/",
+	},
 
-  component: {
-    devServer: {
-      framework: "react",
-      bundler: "vite",
-    },
-  },
+	component: {
+		devServer: {
+			framework: "react",
+			bundler: "vite",
+		},
+	},
 });
--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,14 @@
 export default {
-    preset: "ts-jest",
-    clearMocks: true,
-    coverageProvider: "v8",
-    testMatch: ["**/__tests__/*.test.[jt]s?(x)"],
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-    collectCoverageFrom: ["src/components/*.tsx"],
-    testEnvironment: "jest-environment-jsdom",
-    transformIgnorePatterns: ["/node_modules/", "^.+\\.css$"],
+	preset: "ts-jest",
+	clearMocks: true,
+	coverageProvider: "v8",
+	testMatch: ["**/__tests__/*.test.[jt]s?(x)"],
+	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+	collectCoverageFrom: ["src/components/*.tsx"],
+	testEnvironment: "jest-environment-jsdom",
+	transformIgnorePatterns: ["/node_modules/", "^.+\\.css$"],
+	moduleNameMapper: {
+		"\\.(css|less|scss)$":
+			"<rootDir>/src/components/__tests__/mocks/styleMock.ts",
+	},
 };
